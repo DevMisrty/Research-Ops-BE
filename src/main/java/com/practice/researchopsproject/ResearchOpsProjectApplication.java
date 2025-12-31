4ccd4eba5e339669568d28920255a4ddf1a33960
DevMisrty <dev.mistry.agileinfoways@gmail.com>
2025-12-31 11:47:13 +0530

    initial commits

---
+++ src/main/java/com/practice/researchopsproject/ResearchOpsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.practice.researchopsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchOpsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResearchOpsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/practice/researchopsproject/ResearchOpsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.researchopsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchOpsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
